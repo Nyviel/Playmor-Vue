40add9855a057523164cf2973ec8c387562c3499
Przemysław Kaczmarski <nyviel111@gmail.com>
2024-10-05 17:58:50 +0200

    Added new function to game service

--- src/services/gameService.ts
+++ src/services/gameService.ts
@@ -1,7 +1,7 @@
 import type { Game } from "@/interfaces/Game";
 import { API } from "@/utilities/constants";
 
-export const fetchHotPicks = async (): Promise<Game> => {
+export const fetchHotPicks = async (): Promise<Game[]> => {
 	try {
 		const response = await fetch(`${API}/games`);
 
@@ -12,6 +12,22 @@ export const fetchHotPicks = async (): Promise<Game> => {
 				"Failed to fetch hot picks" +
 					response.status +
 					response.statusText
+			);
+		}
+	} catch (error) {
+		throw new Error("Unexpected error when fetching" + error);
+	}
+};
+
+export const fetchGameById = async (id: number): Promise<Game> => {
+	try {
+		const response = await fetch(`${API}/games/${id}`);
+
+		if (response.ok) {
+			return response.json();
+		} else {
+			throw new Error(
+				"Failed to fetch game" + response.status + response.statusText
 			);
 		}
 	} catch (error) {
