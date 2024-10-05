bc2c8bbf17440fa17c2df9aa5a6e8178e069567f
Przemysław Kaczmarski <nyviel111@gmail.com>
2024-10-05 17:57:34 +0200

    Shadcn configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,57 @@
+const animate = require("tailwindcss-animate");
+
 /** @type {import('tailwindcss').Config} */
-export default {
-	content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+module.exports = {
+	darkMode: ["class"],
+	safelist: ["dark"],
+	prefix: "",
+
+	content: [
+		"./pages/**/*.{ts,tsx,vue}",
+		"./components/**/*.{ts,tsx,vue}",
+		"./app/**/*.{ts,tsx,vue}",
+		"./src/**/*.{ts,tsx,vue}",
+	],
+
 	theme: {
-		extend: {},
+		container: {},
+		extend: {
+			colors: {
+				border: "hsl(var(--border))",
+				input: "hsl(var(--input))",
+				ring: "hsl(var(--ring))",
+				background: "hsl(var(--background))",
+				foreground: "hsl(var(--foreground))",
+				primary: {
+					DEFAULT: "hsl(var(--primary))",
+					foreground: "hsl(var(--primary-foreground))",
+				},
+				secondary: {
+					DEFAULT: "hsl(var(--secondary))",
+					foreground: "hsl(var(--secondary-foreground))",
+				},
+				destructive: {
+					DEFAULT: "hsl(var(--destructive))",
+					foreground: "hsl(var(--destructive-foreground))",
+				},
+				muted: {
+					DEFAULT: "hsl(var(--muted))",
+					foreground: "hsl(var(--muted-foreground))",
+				},
+				accent: {
+					DEFAULT: "hsl(var(--accent))",
+					foreground: "hsl(var(--accent-foreground))",
+				},
+				popover: {
+					DEFAULT: "hsl(var(--popover))",
+					foreground: "hsl(var(--popover-foreground))",
+				},
+				card: {
+					DEFAULT: "hsl(var(--card))",
+					foreground: "hsl(var(--card-foreground))",
+				},
+			},
+		},
 	},
-	plugins: [],
+	plugins: [animate],
 };
