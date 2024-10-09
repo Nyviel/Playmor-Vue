aca229244bf67f15b7388ccf4697481bec036347
Przemysław Kaczmarski <nyviel111@gmail.com>
2024-10-09 18:10:30 +0200

    Imported the vue icons into the app

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,14 @@ import "./assets/main.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import { router } from "./router";
+import * as FaIcons from "oh-vue-icons/icons/fa";
+import * as CoIcons from "oh-vue-icons/icons/co";
+import { addIcons, OhVueIcon } from "oh-vue-icons";
 
+const Fa = Object.values({ ...FaIcons });
+const Co = Object.values({ ...CoIcons });
+addIcons(...Fa, ...Co);
 const app = createApp(App);
+app.component("v-icon", OhVueIcon);
 app.use(router);
 app.mount("#app");
